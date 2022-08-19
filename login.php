c39c0f8cdc4ad0ad1432bec427737aaa1dfd1a06
TSUNODA-web <makojin.deru@icloud.com>
2022-08-19 10:36:02 +0900

    ログイン関係の雛形作成

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>login</title>
+</head>
+
+<body>
+  <header>
+    <h1><a href="">筋トレメモ</a></h1>
+  </header>
+  <div class="form">
+    <div class="form_title">ログインフォーム</div>
+    <div class="form_content">
+      <form action="" method="post">
+        <div class="form_list">
+          <label>お名前</label>
+          <input name="name" type="text" value="">
+          <label>メールアドレス</label>
+          <input name="email" type="email" value="">
+          <label>パスワード</label>
+          <input name="password" type="password" value="">
+          <div><a href="#">会員登録はこちら</a> | <input type="submit" value="登録する" /></div>
+        </div>
+      </form>
+    </div>
+  </div>
+</body>
+
+</html>
