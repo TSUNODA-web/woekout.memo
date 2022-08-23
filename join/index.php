1af0f30d755cd26e0c0267a6c81cf75d95524049
TSUNODA-web <makojin.deru@icloud.com>
2022-08-23 20:36:46 +0900

    css設計中

--- join/index.php
+++ join/index.php
@@ -14,26 +14,23 @@
   <header>
     <h1><a href="">筋トレメモ</a></h1>
   </header>
-  <div class="form">
-    <div class="form_title">会員登録フォーム</div>
-    <div class="form_content">
-      <form action="" method="post">
-        <div class="form_list">
-          <label>名前を入力してください</label>
-          <span class="required">必須</span>
-          <input name="name" type="text" value="">
-          <label>メールアドレスを入力してください</label>
-          <span class="required">必須</span>
-          <input name="email" type="email" value="">
-          <label>パスワードを入力してください</label>
-          <span class="required">必須</span>
-          <input name="password" type="password" value="">
-          <label>パスワードを再入力してください</label>
-          <input name="password" type="password" value="">
-          <input type="submit" name="" value="入力内容確認">
-        </div>
-      </form>
-    </div>
+  <p class="form-title">会員登録フォーム</p>
+  <div class="form-content">
+    <form action="" method="post">
+      <div class="form-list">
+        <label>名前を入力してください</label>
+        <input name="name" type="text" value="">
+        <label>メールアドレスを入力してください</label>
+        <input name="email" type="email" value="">
+        <label>パスワードを入力してください</label>
+        <input name="password" type="password" value="">
+        <label>パスワードを再入力してください</label>
+        <input name="password" type="password" value="">
+      </div class="btn-area">
+      <input type="submit" name="" value="入力内容確認">
+  </div>
+  </div>
+  </form>
   </div>
 </body>
 
