6beb881d78a0fdfbb3ba1e92f345f0c34c46afbe
TSUNODA-web <makojin.deru@icloud.com>
2022-08-26 08:33:13 +0900

    マイページcss設計中

--- join/thanks.php
+++ join/thanks.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../reset.css" />
+  <link rel="stylesheet" href="../style.css" />
+
   <title>会員登録</title>
 </head>
 
@@ -12,11 +15,12 @@
   <header>
     <h1><a href="">筋トレメモ</a></h1>
   </header>
-  <div class="form">
-    <div class="form_title">会員登録フォーム</div>
-    <div class="form_content">
-      <p>登録完了</p>
-      <P><a href="#">ログイン</a></P>
+  <div class="form-title">会員登録フォーム</div>
+  <p>登録が完了しました</p>
+  <div class="content">
+    <a href="../login.php" class="button">ログイン</a>
+  </div>
+
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="reset.css" />
+  <link rel="stylesheet" href="style.css" />
+
   <title>login</title>
 </head>
 
@@ -14,22 +17,25 @@
       <h1><a href="">筋トレメモ</a></h1>
     </div>
   </header>
-  <div class="form">
-    <div class="form_title">ログインフォーム</div>
-    <div class="form_content">
-      <form action="" method="post">
-        <div class="form_list">
-          <label>お名前</label>
-          <input name="name" type="text" value="">
-          <label>メールアドレス</label>
-          <input name="email" type="email" value="">
-          <label>パスワード</label>
-          <input name="password" type="password" value="">
-          <div><a href="#">会員登録はこちら</a> | <input type="submit" value="登録する" /></div>
-        </div>
-      </form>
-    </div>
+  <p class="form-title">ログインフォーム</p>
+  <div class="form-content">
+    <form action="" method="post">
+      <div class="form-list">
+        <label>メールアドレス</label>
+        <input name="email" type="email" value="">
+      </div>
+      <div class="form-list">
+        <label>パスワード</label>
+        <input name="password" type="password" value="">
+      </div>
+      <div class="btn-area">
+        <a href="join/index.php" class="button">会員登録はこちら</a>
+
+        <input type="submit" name="" value="ログイン">
+      </div>
+    </form>
   </div>
+
 </body>
 
 </html>
