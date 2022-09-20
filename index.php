9d2b404e94f0332ce616665b3451038060e3b42b
TSUNODA-web <makojin.deru@icloud.com>
2022-09-20 18:58:25 +0900

    一覧表示できたがバラバラに表示されてしまう

--- index.php
+++ index.php
@@ -3,28 +3,12 @@ session_start();
 require('library.php');
 
 if (isset($_SESSION['id']) && isset($_SESSION['name'])) {
-  $id = $_SESSION['id'];
+  $member_id = $_SESSION['id'];
   $name = $_SESSION['name'];
 } else {
   header('Location: login.php');
   exit();
 }
-
-$db = dbconnect();
-
-$stmt = $db->prepare('select p.id, p.member_id,p.weight, p.created, p.part, p.memo, p.picture, m.name from posts p, members m where m.id=p.member_id order by id desc');
-
-if (!$stmt) {
-  die($db->error);
-}
-$success = $stmt->execute();
-$stmt->bind_result($id, $member_id, $weight, $created, $part, $memo, $picture, $name);
-while ($stmt->fetch());
-var_dump($id, $member_id, $weight, $created, $part, $memo, $picture, $name);
-
-
-
-
 ?>
 
 
@@ -46,37 +30,28 @@ var_dump($id, $member_id, $weight, $created, $part, $memo, $picture, $name);
     <h1><a href="">筋トレメモ</a></h1>
     <ul class="nav-list">
       <li class="nav-list-item">
-        <a href="mypage.php?id=<?php echo h($id); ?>">マイページ</a>
+        <a href="mypage.php?id=<?php echo h($member_id); ?>">マイページ</a>
       </li>
       <li class="nav-list-item">
         <a href="logout.php">ログアウト</a>
       </li>
     </ul>
   </header>
-  <div id="cards">
-    <div class="card">
-      <?php if ($picture) : ?>
-        <div class="picture"><a href="view.php"><img src="picture/<?php echo h($picture); ?>"></a>
-        </div>
-      <?php endif; ?>
-      <div class="description">
-        <p>[部位]<?php echo h($part); ?></p>
-        <br>
-        <p class="day">[投稿日]<?php echo h($created); ?></p>
+  <?php
+  $db = dbconnect();
 
-      </div>
-    </div>
-    <div class=" card" id="card-center">
-      <?php if ($picture) : ?>
-        <div class="picture"><a href="view.php"><img src="picture/<?php echo h($picture); ?>"></a>
-        </div>
-      <?php endif; ?>
-      <div class="description">
-        <p>[部位]<?php echo h($part); ?></p>
-        <br>
-        <p class="day">[投稿日]<?php echo h($created); ?></p>
-      </div>
-    </div>
+  $stmt = $db->prepare('select p.id, p.member_id, p.created, p.part, p.picture from posts p where p.member_id=? order by p.id desc');
+  if (!$stmt) {
+    die($db->error);
+  }
+  $stmt->bind_param('i', $member_id);
+  $success = $stmt->execute();
+  if (!$success) {
+    die($db->error);
+  }
+  $stmt->bind_result($id, $member_id, $created, $part, $picture);
+  while ($stmt->fetch()) :
+  ?>
     <div class="card">
       <?php if ($picture) : ?>
         <div class="picture"><a href="view.php"><img src="picture/<?php echo h($picture); ?>"></a>
@@ -88,44 +63,8 @@ var_dump($id, $member_id, $weight, $created, $part, $memo, $picture, $name);
         <p class="day">[投稿日]<?php echo h($created); ?></p>
       </div>
     </div>
-  </div>
-  <div id="cards">
-    <div class="card">
-      <?php if ($picture) : ?>
-        <div class="picture"><a href="view.php"><img src="picture/<?php echo h($picture); ?>"></a>
-        </div>
-      <?php endif; ?>
-      <div class="description">
-        <p>[部位]<?php echo h($part); ?></p>
-        <br>
-        <p class="day">[投稿日]<?php echo h($created); ?></p>
-      </div>
-    </div>
-    <div class="card" id="card-center">
-      <?php if ($picture) : ?>
-        <div class="picture"><a href="view.php"><img src="picture/<?php echo h($picture); ?>"></a>
-        </div>
-      <?php endif; ?>
-      <div class="description">
-        <p>[部位]<?php echo h($part); ?></p>
-        <br>
-        <p class="day">[投稿日]<?php echo h($created); ?></p>
-      </div>
-    </div>
-    <div class="card">
-      <?php if ($picture) : ?>
-        <div class="picture"><a href="view.php"><img src="picture/<?php echo h($picture); ?>"></a>
-        </div>
-      <?php endif; ?>
-      <div class="description">
-        <p>[部位]<?php echo h($part); ?></p>
-        <br>
-        <p class="day">[投稿日]<?php echo h($created); ?></p>
-      </div>
-    </div>
-  </div>
+  <?php endwhile; ?>
 
-  <a href="memo/post.php?id=<?php echo h($id); ?>" class="button">メモする</a>
 
 
 
--- view.php
+++ view.php
@@ -5,17 +5,27 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../reset.css" />
-  <link rel="stylesheet" href="../style.css" />
-
+  <link rel="stylesheet" href="reset.css" />
+  <link rel="stylesheet" href="style.css" />
   <title>メモ詳細</title>
 </head>
 
 <body>
   <header>
     <h1><a href="">筋トレメモ</a></h1>
+    <ul class="nav-list">
+      <li class="nav-list-item">
+        <a href=" mypage.php">マイページ</a>
+      </li>
+      <li class="nav-list-item">
+        <a href="logout.php">ログアウト</a>
+      </li>
+    </ul>
   </header>
-
+  <div id=view>
+    <div class="picture"><img src="picture/20220918105946_dumbbell_man.png">
+    </div>
+  </div>
 </body>
 
 </html>
