3b163812e2200f557be3e07efc7dda216112335a
TSUNODA-web <makojin.deru@icloud.com>
2022-10-24 15:08:58 +0900

    一覧ページ編集中

--- index.php
+++ index.php
@@ -70,22 +70,29 @@ if ($page > $max_page) {
   ?>
 
   <?php foreach ($result as $memo) { ?>
-    <div id="cards">
-      <div class="card">
-        <?php if ($memo['picture']) : ?>
-          <div class="picture"><a href="detail.php?id=<?php echo $memo['id']; ?>"><img src="picture/<?php echo $memo['picture']; ?>"></a>
-          <?php else : ?>
-            <div class="picture"><a href="detail.php?id=<?php echo $memo['id']; ?>"><img src="empty_image/20200501_noimage.jpg"></a>
+    <main>
+      <section id="content1">
+        <div class="wrapper">
+          <div id="cards">
+            <div class="card">
+              <?php if ($memo['picture']) : ?>
+                <div class="picture"><a href="detail.php?id=<?php echo $memo['id']; ?>"><img src="picture/<?php echo $memo['picture']; ?>"></a>
+                <?php else : ?>
+                  <div class="picture"><a href="detail.php?id=<?php echo $memo['id']; ?>"><img src="empty_image/20200501_noimage.jpg"></a>
+                  </div>
+                <?php endif; ?>
+                <div class="description">
+                  <p>[部位]<?php echo $memo['part']; ?></p>
+                  <br>
+                  <p class="day">[投稿日]<?php echo $memo['created']; ?></p>
+                </div>
+                </div>
             </div>
-          <?php endif; ?>
-          <div class="description">
-            <p>[部位]<?php echo $memo['part']; ?></p>
-            <br>
-            <p class="day">[投稿日]<?php echo $memo['created']; ?></p>
           </div>
-          </div>
-      </div>
-    </div>
+
+        </div>
+      </section>
+    </main>
   <?php } ?>
   <div class="btn-area">
     <div class="pagination">
