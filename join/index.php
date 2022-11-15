90fbe482a2a68cc47447120212cee818ef01494c
TSUNODA-web <makojin.deru@icloud.com>
2022-11-15 18:53:31 +0900

    a

--- join/index.php
+++ join/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require('./');
+require('../library.php');
 
 if (isset($_GET['action']) && $_GET['action'] === 'rewrite' && isset($_SESSION['form'])) {
   $form = $_SESSION['form'];
